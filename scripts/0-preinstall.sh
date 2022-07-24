6768a6a779ca66125d04defc499611e364b8c06b
Kornel Chlebovics <kchlebovics@mailbox.org>
2022-07-24 20:57:30 +0300

    luks fix

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -123,13 +123,13 @@ elif [[ "${FS}" == "ext4" ]]; then
 elif [[ "${FS}" == "luks" ]]; then
     mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
 # enter luks password to cryptsetup and format root partition
-    echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat ${partition3} -
+    echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat --type luks1 ${partition3} -
 # open luks container and ROOT will be place holder 
     echo -n "${LUKS_PASSWORD}" | cryptsetup open ${partition3} ROOT -
 # now format that container
-    mkfs.btrfs -L ROOT ${partition3}
+    mkfs.btrfs -L ROOT /dev/mapper/ROOT
 # create subvolumes for btrfs
-    mount -t btrfs ${partition3} /mnt
+    mount -t btrfs /dev/mapper/ROOT /mnt
     subvolumesetup
 # store uuid of encrypted partition for grub
     echo ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value ${partition3}) >> $CONFIGS_DIR/setup.conf
